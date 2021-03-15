001e1ad1244e234a67c36f2dd2a64a99dcb86e5d
mireille1999 <muhawenimana920@daviscollege.com>
2021-03-15 22:12:23 +0200

    added sources file

---
+++ sources.py
@@ -0,0 +1,10 @@
+class Source:
+
+    def __init__ (self,category,id,name,description,url):
+
+        self.category = category
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        
